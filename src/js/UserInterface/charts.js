1655f2a1b36c689db1ae5823deff314c181cf16d
Michael.Samula <msamula@web.de>
2022-12-05 13:43:02 +0100

    timeChart funktioniert
    configure

--- src/js/UserInterface/charts.js
+++ src/js/UserInterface/charts.js
@@ -1,6 +1,14 @@
 import Chart from 'chart.js/auto';
 
-let slagChart;
+let slagChart, timeChart;
+
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update('none');
+}
 
 export function createCharts(){
 
@@ -30,10 +38,63 @@ export function createCharts(){
                 }
             }
         }
-    })
+    });
+
+    timeChart = new Chart( document.getElementById('timeChart') , {
+        type: 'bar',
+        data: {
+            labels: [''],
+            datasets: [{
+                label: 'Slag',
+                data: [],
+                borderWidth: 1,
+                backgroundColor: '#FF0000',
+                color: '#FFF'
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    max: 100,
+                    min: 0,
+                    grid: {
+                        display: false
+                    }
+                },
+                x: {
+                    grid: {
+                        display: false
+                    }
+                }
+            }
+        }
+    });
+
+    for (let i = 0; i < 100; i++) {
+        addData(timeChart, '', 0);
+    }
 }
 
 export function updateSlagChart(slag, totalSlag){
+
+    /*SLAG CHART*/
+    //update data
     slagChart.data.datasets[0].data = [slag, totalSlag];
+
+
+    /*TIME CHART*/
+    let date = new Date();
+
+    //add data
+    timeChart.data.labels.push(`${(date.getMinutes()<10?'0':'') + date.getMinutes()}:${(date.getSeconds()<10?'0':'') + date.getSeconds()}`);
+    timeChart.data.datasets[0].data.push(slag);
+
+    //remove data
+    if(timeChart.data.labels.length > 100){
+        timeChart.data.labels.shift();
+        timeChart.data.datasets[0].data.shift();
+    }
+
     slagChart.update('none');
+    timeChart.update('none');
 }
