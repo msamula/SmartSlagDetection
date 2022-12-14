b8e44780c6579bffa06b991077d63c91f74d8e69
msamu <msamula@web.de>
2022-12-14 14:02:08 +0100

    lua not min -> max

--- public/job/SlagDetection.lua
+++ public/job/SlagDetection.lua
@@ -3,8 +3,8 @@
 -- #INFO#
 -- Job: SlagDetection
 -- Creation date: 12-02-22,09:44:35
--- Modified date: 12-12-22,05:00:48
--- MD5: 6155e4ab279cf97cfd8d7d3a1237b7e9
+-- Modified date: 12-14-22,09:16:11
+-- MD5: 1431499d50f6fb022ebde1e88a27b0c6
 -- #END INFO#
 
 --GENERATED_CODE DO NOT EDIT
@@ -40,7 +40,7 @@ function evaluate()
     Results["AOI_1_Count_Bin"] = false
     Results["AOI_0_Count"] = eval_AOI_0:count()
     Results["AOI_0_Count_Bin"] = false
-    Results["MaxTemp"] = eval_AOI_0:min()
+    Results["MaxTemp"] = eval_AOI_0:max()
     Results["MaxTemp_Bin"] = false
     Results["TotalSlag"] = eval_AOI_0:mean()
     Results["TotalSlag_Bin"] = false
@@ -48,35 +48,25 @@ function evaluate()
     ---GENERATED_CODE_END
 
     --USER_EVAL_POST_START
-	
-		Results["AOI_0_Result"] = ((Results["AOI_0_Count"]:	getValue() / Results["AOI_0_Area"]:getValue()) * 100) + 273.15
-		
-		if (Results["AOI_0_Result"]:getValue() < (0.1 + 273.15))then
-		
-			SlagSumOld = 0 
-			SumGesOld  = 0 
-			
-		end
-		
-		Results["AOI_1_Result"] = ((Results["AOI_1_Count"]:getValue() / Results["AOI_0_Count"]:getValue()) * 100) + 273.15
-			
-		SlagSumNew = Results["AOI_1_Count"]:getValue()
-		SlagSumOld = SlagSumNew + SlagSumOld
-			
-		SumGesNew = Results["AOI_0_Count"]:getValue()
-		SumGesOld = SumGesNew + SumGesOld
-		
-		if (SumGesOld > 50000)then
-		
-			Results["TotalSlag"] = ((SlagSumOld / SumGesOld) * 100) + 273.15
-		
-		else
-		
-			Results["TotalSlag"] = 0 + 273.15
-		
-		end
+    Results["AOI_0_Result"] = ((Results["AOI_0_Count"]:getValue() / Results["AOI_0_Area"]:getValue()) * 100) + 273.15
 
+    if (Results["AOI_0_Result"]:getValue() < (0.1 + 273.15)) then
+        SlagSumOld = 0
+        SumGesOld = 0
+    end
+
+    Results["AOI_1_Result"] = ((Results["AOI_1_Count"]:getValue() / Results["AOI_0_Count"]:getValue()) * 100) + 273.15
+
+    SlagSumNew = Results["AOI_1_Count"]:getValue()
+    SlagSumOld = SlagSumNew + SlagSumOld
+
+    SumGesNew = Results["AOI_0_Count"]:getValue()
+    SumGesOld = SumGesNew + SumGesOld
+
+    if (SumGesOld > 50000) then
+        Results["TotalSlag"] = ((SlagSumOld / SumGesOld) * 100) + 273.15
+    else
+        Results["TotalSlag"] = 0 + 273.15
+    end
     --USER_EVAL_POST_END
 end
-
-
